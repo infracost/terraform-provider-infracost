0edaf61a1bddac0e5660cab2cf546fd0482fdb4f
Sina Bakhtiari <sinabakh44@gmail.com>
2020-09-08 01:08:24 +0430

    Add DynamoDB to providers

--- infracost/provider.go
+++ infracost/provider.go
@@ -10,6 +10,7 @@ func Provider() terraform.ResourceProvider {
 		DataSourcesMap: map[string]*schema.Resource{
 			"infracost_aws_nat_gateway":     dataSourceAwsNatGateway(),
 			"infracost_aws_lambda_function": dataSourceAwsLambdaFunction(),
+			"infracost_aws_dynamodb_table":  dataSourceAwsDynamoDBTable(),
 		},
 	}
 }
