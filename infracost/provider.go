584c381eb1b627b88389d24545147680771de7a9
siahaanbernard <bernardfounder@gmail.com>
2020-10-22 20:00:15 +0700

    add support for codebuild resources.

--- infracost/provider.go
+++ infracost/provider.go
@@ -8,10 +8,11 @@ import (
 func Provider() terraform.ResourceProvider {
 	return &schema.Provider{
 		DataSourcesMap: map[string]*schema.Resource{
-			"infracost_aws_nat_gateway":     dataSourceAwsNatGateway(),
-			"infracost_aws_lambda_function": dataSourceAwsLambdaFunction(),
-			"infracost_aws_dynamodb_table":  dataSourceAwsDynamoDBTable(),
-			"infracost_aws_sqs_queue": dataSourceAwsSQSQueue(),
+			"infracost_aws_nat_gateway":       dataSourceAwsNatGateway(),
+			"infracost_aws_lambda_function":   dataSourceAwsLambdaFunction(),
+			"infracost_aws_dynamodb_table":    dataSourceAwsDynamoDBTable(),
+			"infracost_aws_sqs_queue":         dataSourceAwsSQSQueue(),
+			"infracost_aws_codebuild_project": dataSourceAwsCodebuildProject(),
 		},
 	}
 }
