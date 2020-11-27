371246a2fb297fc45dccdb00964a79cdbad63643
Rob Houghton <rob.houghton@cdl.co.uk>
2020-11-27 18:20:41 +0000

    Formatting

--- infracost/data_source_aws_cloudwatch_log_group.go
+++ infracost/data_source_aws_cloudwatch_log_group.go
@@ -1,17 +1,17 @@
 package infracost
 
 import (
-    "github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/schema"
 )
 
 func dataSourceAwsCloudwatchLogGroup() *schema.Resource {
-    return &schema.Resource{
-        Read: dataSourceRead,
-        Schema: map[string]*schema.Schema{
-            "resources": resourcesSchema(),
-            "monthly_gb_data_ingestion": usageSchema(),
-            "monthly_gb_data_storage": usageSchema(),
-            "monthly_gb_data_scanned": usageSchema(),
-        },
-    }
+	return &schema.Resource{
+		Read: dataSourceRead,
+		Schema: map[string]*schema.Schema{
+			"resources":                 resourcesSchema(),
+			"monthly_gb_data_ingestion": usageSchema(),
+			"monthly_gb_data_storage":   usageSchema(),
+			"monthly_gb_data_scanned":   usageSchema(),
+		},
+	}
 }
--- infracost/data_source_aws_cloudwatch_log_group_test.go
+++ infracost/data_source_aws_cloudwatch_log_group_test.go
@@ -1,33 +1,33 @@
 package infracost
 
 import (
-    "testing"
+	"testing"
 
-    "github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/helper/resource"
 )
 
 func TestAwsCloudwatchLogGroup(t *testing.T) {
-    name := "data.infracost_aws_cloudwatch_log_group.log_group"
+	name := "data.infracost_aws_cloudwatch_log_group.log_group"
 
-    resource.Test(t, resource.TestCase{
-        PreCheck:  func() {},
-        Providers: testAccProviders,
-        Steps: []resource.TestStep{
-            {
-                Config: testAwsCloudwatchLogGroupConfig(),
-                Check: resource.ComposeAggregateTestCheckFunc(
-                    resource.TestCheckResourceAttr(name, "resources.#", "2"),
-                    testCheckResourceAttrValue(name, "monthly_gb_data_ingestion", 1000),
-                    testCheckResourceAttrValue(name, "monthly_gb_data_storage", 500),
-                    testCheckResourceAttrValue(name, "monthly_gb_data_scanned", 100),
-                ),
-            },
-        },
-    })
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() {},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAwsCloudwatchLogGroupConfig(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(name, "resources.#", "2"),
+					testCheckResourceAttrValue(name, "monthly_gb_data_ingestion", 1000),
+					testCheckResourceAttrValue(name, "monthly_gb_data_storage", 500),
+					testCheckResourceAttrValue(name, "monthly_gb_data_scanned", 100),
+				),
+			},
+		},
+	})
 }
 
 func testAwsCloudwatchLogGroupConfig() string {
-    return `
+	return `
 		data "infracost_aws_cloudwatch_log_group" "log_group" {
 			resources = list("log_group_1", "log_group_2")
 
