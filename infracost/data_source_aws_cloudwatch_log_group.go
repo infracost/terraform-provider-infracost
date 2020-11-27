0de8335c91f3febfa42e3fcd1f5197647dc7372a
Rob Houghton <rob.houghton@cdl.co.uk>
2020-11-27 18:17:33 +0000

    Support for cloudwatch logs

---
+++ infracost/data_source_aws_cloudwatch_log_group.go
@@ -0,0 +1,17 @@
+package infracost
+
+import (
+    "github.com/hashicorp/terraform/helper/schema"
+)
+
+func dataSourceAwsCloudwatchLogGroup() *schema.Resource {
+    return &schema.Resource{
+        Read: dataSourceRead,
+        Schema: map[string]*schema.Schema{
+            "resources": resourcesSchema(),
+            "monthly_gb_data_ingestion": usageSchema(),
+            "monthly_gb_data_storage": usageSchema(),
+            "monthly_gb_data_scanned": usageSchema(),
+        },
+    }
+}
--- infracost/provider.go
+++ infracost/provider.go
@@ -12,6 +12,7 @@ func Provider() terraform.ResourceProvider {
             "infracost_aws_apigatewayv2_api":     dataSourceAwsApiGatewayV2Api(),
             "infracost_aws_codebuild_project":    dataSourceAwsCodebuildProject(),
             "infracost_aws_dynamodb_table":       dataSourceAwsDynamoDBTable(),
+            "infracost_aws_cloudwatch_log_group": dataSourceAwsCloudwatchLogGroup(),
             "infracost_aws_lambda_function":      dataSourceAwsLambdaFunction(),
             "infracost_aws_nat_gateway":          dataSourceAwsNatGateway(),
             "infracost_aws_sqs_queue":            dataSourceAwsSQSQueue(),
